8668ca003b37a7b45900091057b0f7a2879152f3
barbara-musial <basia.musial22@op.pl>
2021-11-20 23:04:10 +0100

    Creating set timer function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let timers = ['Timer0', 'Timer1'];
-let timerNumber = 2;
+let timers = ['Timer0'];
+let timerNumber = 1;
 
 document
   .getElementById('addTimerButton')
@@ -17,7 +17,10 @@ function changeDescription(tNumb) {
   const descLabel = document.querySelector(`.d-${tNumb}`);
   const editDescButton = document.querySelector(`.edb-${tNumb}`);
 
-  if (descChangeInput.style.display === 'inline') {
+  if (
+    descChangeInput.style.display === 'inline' &&
+    descChangeInput.value !== ''
+  ) {
     const description = document.querySelector(`.dc-${tNumb}`).value;
 
     descLabel.textContent = description;
@@ -25,7 +28,7 @@ function changeDescription(tNumb) {
 
     descChangeInput.style.display = descChangeButton.style.display = 'none';
     descLabel.style.display = editDescButton.style.display = 'inline';
-  } else {
+  } else if (descLabel.style.display === 'inline') {
     const description = document.querySelector(`.d-${tNumb}`).textContent;
 
     descChangeInput.value = description;
@@ -34,4 +37,65 @@ function changeDescription(tNumb) {
     descLabel.style.display = editDescButton.style.display = 'none';
     descChangeInput.style.display = descChangeButton.style.display = 'inline';
   }
+
+  const hideDescriptionElements = function (tNumb) {
+    const descChangeInput = document.querySelector(`.dc-${tNumb}`);
+    const descChangeButton = document.querySelector(`.dcb-${tNumb}`);
+    const descLabel = document.querySelector(`.d-${tNumb}`);
+    const editDescButton = document.querySelector(`.edb-${tNumb}`);
+
+    if (
+      descChangeInput.value === '' &&
+      descChangeButton.style.display === 'inline' &&
+      descChangeInput.style.display === 'inline'
+    ) {
+      descLabel.style.display =
+        editDescButton.style.display =
+        descChangeInput.style.display =
+        descChangeButton.style.display =
+          'none';
+    }
+  };
+}
+
+hideDescriptionElements(0);
+function setTimer(tNumb) {
+  let hours = document.querySelector(`.h-${tNumb}`);
+  let minutes = document.querySelector(`.m-${tNumb}`);
+  let seconds = document.querySelector(`.s-${tNumb}`);
+
+  if (seconds.value == 0 && minutes.value == 0 && hours.value == 0) {
+    alert(`You didn't set any values`);
+  } else {
+    hideDescriptionElements(0);
+    const timer = window.setInterval(() => {
+      if (seconds.value == 0 && minutes.value == 0 && hours.value > 0) {
+        seconds.value = 60;
+        minutes.value = 59;
+        hours.value--;
+      }
+
+      if (seconds.value == 0 && minutes.value > 0) {
+        seconds.value = 60;
+        minutes.value--;
+      }
+
+      seconds.value--;
+      if (seconds.value == 0 && minutes.value == 0 && hours.value == 0) {
+        window.clearInterval(timer);
+      }
+    }, 1000);
+  }
+  // if (seconds.value > 0) {
+  //   const sec = window.setInterval(function () {
+  //     seconds.value--;
+  //     if (seconds.value == 0 && minutes.value == 0) {
+  //       window.clearInterval(sec);
+  //     }
+  //     if (seconds.value == 0 && minutes.value > 0) {
+  //       ;
+  //       minutes.value--;
+  //     }
+  //   }, 1000);
+  // }
 }
