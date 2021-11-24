7f1d988ce684d67754c9dccc89094b8df39019e7
barbara-musial <basia.musial22@op.pl>
2021-11-24 20:49:46 +0100

    Changing html in addTimer function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-let timers = ['Timer0'];
-let timerNumber = 1;
+let i = 1;
 
 function changeDescription(tNumb) {
   const descChangeInput = document.querySelector(`.dc-${tNumb}`);
@@ -10,55 +9,34 @@ function changeDescription(tNumb) {
   const editDescButton = document.querySelector(`.edb-${tNumb}`);
 
   if (
-    descChangeInput.style.display === 'inline' &&
-    descChangeInput.value !== ''
+    descChangeInput.style.display === "inline" &&
+    descChangeInput.value !== ""
   ) {
     const description = document.querySelector(`.dc-${tNumb}`).value;
 
     descLabel.textContent = description;
-    descChangeInput.value = '';
+    descChangeInput.value = "";
 
-    descChangeInput.style.display = descChangeButton.style.display = 'none';
-    descLabel.style.display = editDescButton.style.display = 'inline';
-  } else if (descLabel.style.display === 'inline') {
+    descChangeInput.style.display = descChangeButton.style.display = "none";
+    descLabel.style.display = editDescButton.style.display = "inline";
+  } else if (descLabel.style.display === "inline") {
     const description = document.querySelector(`.d-${tNumb}`).textContent;
 
     descChangeInput.value = description;
-    descLabel.textContent = '';
+    descLabel.textContent = "";
 
-    descLabel.style.display = editDescButton.style.display = 'none';
-    descChangeInput.style.display = descChangeButton.style.display = 'inline';
+    descLabel.style.display = editDescButton.style.display = "none";
+    descChangeInput.style.display = descChangeButton.style.display = "inline";
   }
-}
-
-function timerSet(tNumb) {
-  const descChangeInput = document.querySelector(`.dc-${tNumb}`);
-  const descChangeButton = document.querySelector(`.dcb-${tNumb}`);
-  const descLabel = document.querySelector(`.d-${tNumb}`);
-  const editDescButton = document.querySelector(`.edb-${tNumb}`);
-  const setButton = document.querySelector(`.sb-${tNumb}`);
-  const pauseButton = document.querySelector(`.pb-${tNumb}`);
-  const deleteButton = document.querySelector(`.db-${tNumb}`);
-
-  if (
-    descChangeInput.value === '' &&
-    descChangeInput.style.display === 'inline' &&
-    descChangeButton.style.display === 'inline'
-  ) {
-    descLabel.style.display =
-      editDescButton.style.display =
-      descChangeInput.style.display =
-      descChangeButton.style.display =
-        'none';
-  } else {
-    editDescButton.style.display = 'none';
-  }
-
-  setButton.style.display = 'none';
-  pauseButton.style.display = deleteButton.style.display = 'inline';
 }
 
 function timerInterval(tNumb) {
+  const descChangeInput = document.querySelector(`.dc-${tNumb}`);
+  const descChangeButton = document.querySelector(`.dcb-${tNumb}`);
+  const editDescButton = document.querySelector(`.edb-${tNumb}`);
+  const setButton = document.querySelector(`.sb-${tNumb}`);
+  const deleteButton = document.querySelector(`.db-${tNumb}`);
+  const pauseButton = document.querySelector(`.pb-${tNumb}`);
   let hours = document.querySelector(`.h-${tNumb}`);
   let minutes = document.querySelector(`.m-${tNumb}`);
   let seconds = document.querySelector(`.s-${tNumb}`);
@@ -83,7 +61,18 @@ function timerInterval(tNumb) {
         window.clearInterval(timer);
       }
     }, 1000);
-    timerSet(tNumb);
+
+    hours.setAttribute("readonly", true);
+    minutes.setAttribute("readonly", true);
+    seconds.setAttribute("readonly", true);
+
+    setButton.style.display =
+      descChangeInput.style.display =
+      descChangeButton.style.display =
+      editDescButton.style.display =
+        "none";
+    pauseButton.style.display = deleteButton.style.display = "inline";
+
     return timer;
   }
 }
@@ -94,16 +83,16 @@ function setTimer(tNumb) {
 
   const timer = timerInterval(tNumb);
 
-  document.querySelector(`.pb-${tNumb}`).addEventListener('click', () => {
-    pauseButton.style.display = 'none';
-    resumeButton.style.display = 'inline';
+  document.querySelector(`.pb-${tNumb}`).addEventListener("click", () => {
+    pauseButton.style.display = "none";
+    resumeButton.style.display = "inline";
 
     window.clearInterval(timer);
   });
 
-  document.querySelector(`.rb-${tNumb}`).addEventListener('click', () => {
-    resumeButton.style.display = 'none';
-    pauseButton.style.display = 'inline';
+  document.querySelector(`.rb-${tNumb}`).addEventListener("click", () => {
+    resumeButton.style.display = "none";
+    pauseButton.style.display = "inline";
     timerInterval(tNumb);
   });
 }
@@ -115,28 +104,66 @@ function deleteTimer(tNumb) {
 
 document
   .querySelector(`#addTimerButton`)
-  .addEventListener('click', function () {
-    const n = timers.length;
+  .addEventListener("click", function () {
     const addTimerButton = document.querySelector(`#addTimerButton`);
     const html = `
-    <form class="timer t-${n}">
-      <input type="text" placeholder="Set description (optional)" style="display: inline" class="desc-change dc-${n}"/>
-      <input type="button" value="✔" onclick="changeDescription(${n})" style="display: inline" class="desc-change-button dcb-${n}"/>
-      <label style="display: none" class="description d-${n}"></label>
-      <input type="button" value="✍" onclick="changeDescription(${n})" style="display: none" class="edit-desc-button edb-${n}"/><br />
-      <input type="number" min="0" max="24" value="0" class="hours h-${n}"/>
+    <form class="timer t-${i}">
+      <input
+        type="text"
+        placeholder="Set description (optional)"
+        style="display: inline"
+        class="desc-change dc-${i}"
+      />
+      <input
+        type="button"
+        value="✔"
+        onclick="changeDescription(${i})"
+        style="display: inline"
+        class="desc-change-button dcb-${i}"
+      />
+      <label style="display: none" class="description d-${i}"></label>
+      <input
+        type="button"
+        value="✍"
+        onclick="changeDescription(${i})"
+        style="display: none"
+        class="edit-desc-button edb-${i}"
+      /><br />
+      <input type="number" min="0" max="24" value="0" class="h-${i}" />
       <label> H </label>
-      <input type="number" min="0" max="59" value="0" class="minutes m-${n}" />
+      <input type="number" min="0" max="59" value="0" class="m-${i}" />
       <label> M </label>
-      <input type="number" min="0" max="59" value="0" class="seconds s-${n}" />
+      <input type="number" min="0" max="59" value="0" class="s-${i}" />
       <label> S </label><br />
-      <input type="button" value="SET" onclick="setTimer(${n})" style="display: inline" class="set-button sb-${n}"/>
-      <input type="button" value="PAUSE" style="display: none" class="pause-button pb-${n}"/>
-      <input type="button" value="RESUME" style="display: none" class="resume-button rb-${n}"/>
-      <input type="button" value="DELETE" onclick="deleteTimer(${n})" style="display: none" class="delete-button db-${n}"/>
+      <input
+        type="button"
+        value="SET"
+        onclick="setTimer(${i})"
+        style="display: inline"
+        class="set-button sb-${i}"
+      />
+      <input
+        type="button"
+        value="PAUSE"
+        style="display: none"
+        class="act-buttons pb-${i}"
+      />
+      <input
+        type="button"
+        value="RESUME"
+        style="display: none"
+        class="act-buttons rb-${i}"
+      />
+      <input
+        type="button"
+        value="DELETE"
+        onclick="deleteTimer(${i})"
+        style="display: none"
+        class="act-buttons db-${i}"
+      />
     </form>
-    <br/>`;
+    <br />`;
 
-    timers.push(`Timer${n}`);
-    addTimerButton.insertAdjacentHTML('beforebegin', html);
+    i++;
+    addTimerButton.insertAdjacentHTML("beforebegin", html);
   });
