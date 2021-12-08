f49e35d0f6af4fa60ef1b4be0f2ba399d1375328
barbara-musial <basia.musial22@op.pl>
2021-12-08 21:27:51 +0100

    Refactor code

--- script.js
+++ script.js
@@ -2,87 +2,53 @@
 
 // CONTAINERS
 const timersContainer = document.querySelector(".timers");
-const timerContainer = document.querySelector(".timer");
 
-// SETTING TIMER
-const addNewTimerForm = document.querySelector("#addTimerButton");
-const setButton = document.querySelectorAll(".set-button");
-// Timers values
-const hours = document.querySelector(".hours");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
-// Description buttons
-const setDescriptionButton = document.querySelector(".desc-change-button");
-const editDescriptionButton = document.querySelector(".edit-desc-button");
-// Description storage elements
-const changeDescriptionInput = document.querySelector(".desc-change");
-const descriptionLabel = document.querySelector(".descrption");
+// BUTTONS
+const addNewTimerFormButton = document.querySelector("#addTimerButton");
 
 // TIMERS BUTTONS
 const pauseButton = document.querySelector(".pause-button");
 const resumeButton = document.querySelector(".resume-button");
 const deleteButton = document.querySelector(".delete-button");
 
-class App {
-  constructor() {
-    addNewTimerForm.addEventListener("click", this._addNewTimerForm);
-    setButton.forEach((sb) =>
-      sb.addEventListener("click", this._createNewTimer)
-    );
-  }
+let id = 1;
 
-  _createNewTimer() {
-    const h = hours.value;
-    const m = minutes.value;
-    const s = seconds.value;
-    const timer = new Timer(h, m, s);
-    console.log(timer);
-  }
-
-  _addNewTimerForm() {
-    const html = `
-    <form class="timer">
-      <input type="text" placeholder="Set description (optional)" style="display: inline" class="desc-change"/>
-      <input type="button" value="✔" style="display: inline" class="desc-change-button"/>
-      <label style="display: none" class="description"></label>
-      <input type="button" value="✍" style="display: none" class="edit-desc-button"/><br/>
-      <input type="number" min="0" max="24" value="0" class="hours"/>
-      <label> H </label>
-      <input type="number" min="0" max="59" value="0" class="minutes"/>
-      <label> M </label>
-      <input type="number" min="0" max="59" value="0" class="seconds"/>
-      <label> S </label><br/>
-      <input type="button" value="SET" style="display: inline" class="set-button"/>
-      <input type="button" value="PAUSE" style="display: none" class="pause-button"/>
-      <input type="button" value="RESUME" style="display: none" class="resume-button"/>
-      <input type="button" value="DELETE" style="display: none" class="delete-button"/>
-    </form>`;
-
-    timersContainer.insertAdjacentHTML("beforeend", html);
-  }
-}
-const app = new App();
-
+// CLASS
 class Timer {
-  #id = `${Date.now()}`.slice(-10);
-  constructor(hours, minutes, seconds) {
-    this.hours = hours;
-    this.minutes = minutes;
-    this.seconds = seconds;
+  constructor(id) {
+    this.id = id;
+    this.timerContainer = document.querySelector(`.t-${this.id}`);
 
-    this._setInterval();
-  }
+    // Timers values
+    this.hours = this.timerContainer.querySelector(".hours");
+    this.minutes = this.timerContainer.querySelector(".minutes");
+    this.seconds = timersContainer.querySelector(".seconds");
 
-  _getId() {
-    return this.#id;
+    // Description elements
+    this.descriptionInput = this.timerContainer.querySelector(".desc-change");
+    this.descriptionLabel = this.timerContainer.querySelector(".desc-change");
+    this.editDescriptionButton =
+      this.timerContainer.querySelector(".edit-desc-button");
+
+    // Action buttons
+    this.pauseButton = this.timerContainer.querySelector(".pause-button");
+    this.resumeButton = this.timerContainer.querySelector(".resume-button");
+    this.deleteButton = this.timerContainer.querySelector(".delete-button");
+
+    // Call immediately:
+    this._setDescription()._setInterval();
   }
 
   _calcTimeInSec() {
-    return (this.time =
-      this.hours * 60 * 60 + this.minutes * 60 + this.seconds);
+    return +(
+      this.hours.value * 60 * 60 +
+      this.minutes.value * 60 +
+      this.seconds.value
+    );
   }
 
   _setInterval() {
+    console.log(this.descriptionInput);
     this.time = this._calcTimeInSec();
     console.log(this.time);
   }
@@ -91,9 +57,52 @@ class Timer {
 
   _togglePauseResume() {}
 
+  _setDescription() {
+    this.descriptionInput.value = this.descriptionLabel.value;
+    console.log(this.descriptionLabel.value);
+    this.descriptionInput.style.display = "none";
+    this.descriptionLabel.style.display = "inline";
+    return this;
+  }
+
+  _editDescription() {}
+
   pause(e) {}
 
   resume(e) {}
 
   delete(e) {}
 }
+
+// FUNCTIONS
+
+function addNewTimer() {
+  const html = `
+  <form class="timer t-${id}">
+    <input type="text" placeholder="Set description (optional)" style="display: inline" class="desc-change"/>
+    <label style="display: none" class="description"></label>
+    <input type="button" value="✍" style="display: none" class="edit-desc-button"/><br/>
+    <input type="number" min="0" max="24" value="0" class="hours"/>
+    <label> H </label>
+    <input type="number" min="0" max="59" value="0" class="minutes"/>
+    <label> M </label>
+    <input type="number" min="0" max="59" value="0" class="seconds"/>
+    <label> S </label><br/>
+    <input type="button" value="SET" style="display: inline" class="set-button"/>
+    <input type="button" value="PAUSE" style="display: none" class="pause-button"/>
+    <input type="button" value="RESUME" style="display: none" class="resume-button"/>
+    <input type="button" value="DELETE" style="display: none" class="delete-button"/>
+  </form>`;
+
+  timersContainer.insertAdjacentHTML("beforeend", html);
+  document
+    .querySelector(`.t-${id}`)
+    .querySelector(".set-button")
+    .addEventListener("click", () => {
+      new Timer(id);
+      id++;
+    });
+}
+
+// EVENT HANDLERS
+addNewTimerFormButton.addEventListener("click", addNewTimer);
